9241fa86bea1a037ee97ebe8ecf69b7c094ca24f
yaxh.xv <yashnawgiri45@gmail.com>
2024-05-25 12:10:18 +0530

    added a list of components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Outlet } from "react-router-dom";
+import { CssBaseline } from "@mui/material";
+ 
+ 
 import './App.css'
 
 function App() {
@@ -8,7 +10,8 @@ function App() {
 
   return (
     <>
-     
+     <CssBaseline />
+     <Outlet />
     </>
   )
 }
